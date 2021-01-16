b5c2fae76525cc3f33e10b13f5a2ad21e849daf8
Lanlanluuu <xww1748@gmail.com>
2021-01-17 00:16:32 +0800

    Remove token

--- lanlanlu_bot.py
+++ lanlanlu_bot.py
@@ -4,7 +4,7 @@
 import discord
 from DrugBot import runLoki as drugbot
 
-DISCORD_TOKEN="Nzg5Mzc1MDAzMDE5NDQ0MjY0.X9xIwg.tdQ0DOZ8fs4eQfR3I4EpZcehmWs"
+DISCORD_TOKEN=""
 DISCORD_GUILD="Droidtown Linguistics Tech."
 BOT_NAME = "DrugBot"
 
